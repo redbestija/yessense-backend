0c89b9bd64e5e45073c741eb6cb2977410fdead7
Evgenia Litvinova (ss) <samochadina@gmail.com>
2014-05-21 16:35:21 +0300

    Fix for unrelated own optinos select

--- app/controllers/ICQA.java
+++ app/controllers/ICQA.java
@@ -609,7 +609,7 @@ public class ICQA extends Controller {
 				"union " +
 				"SELECT q.id as QuestionID, q.question_prompt as Prefix, q.question_text as QuestionText, loc.description as OptionValue, loc_type.type as CategoryValue " +
 				"FROM question q, icqa.location loc left join loc_type loc_type on (loc.type_id= loc_type.id) " +
-				"where q.id = 2 and ((loc.instance_id = ? or loc.user_id = (SELECT id FROM user WHERE username = ? LIMIT 1))) " +
+				"where q.id = 2 and loc.instance_id = ? and ((loc.user_id is null) or loc.user_id = (SELECT id FROM user WHERE username = ? LIMIT 1)) " +
 				"order by questionID, CategoryValue";
 			PreparedStatement statement = conn.prepareStatement(query);
 			statement.setInt(1, instanceID);
